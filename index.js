df50f914955c80159fc7483987aceea3afc51cc2
Shreyance Bodh <shreyancebodh@gmail.com>
2025-11-29 18:19:24 +0530

    cors added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -9,6 +10,7 @@ const connectDB = require("./config/db");
 connectDB();
 
 // middlewares
+app.use(cors());
 app.use(express.json());
 
 app.get("/healthcheck", (req, res) => {
